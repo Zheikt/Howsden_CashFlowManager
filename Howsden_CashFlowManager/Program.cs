6cf1f996476ff925924c4f72478deb2b921c1eca
Samuel Howsden <samuel.howsden@student.centralia.edu>
2021-06-02 12:07:53 -0700

    Add project files.

---
+++ Howsden_CashFlowManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Howsden_CashFlowManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
